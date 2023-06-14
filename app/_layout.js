74737ddf0b4456b8648180b070272840a1027022
Minh Vy ha <minhvy.ha@outlook.com>
2023-06-14 17:53:00 +1000

    init

---
+++ app/_layout.js
@@ -0,0 +1,6 @@
+import {Stack} from 'expo-router'
+
+function Layout(){
+  return <Stack />
+}
+export default Layout
---
+++ app/index.js
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native'
+
+function Home(){
+  return (
+    <View>
+      <Text>Hello, world</Text>
+    </View>
+  )
+}
+
+export default Home
