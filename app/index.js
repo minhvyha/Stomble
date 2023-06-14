12f3942e0f2138cf9a56079fbd73f57bb516a69a
Minh Vy ha <minhvy.ha@outlook.com>
2023-06-14 20:56:49 +1000

    fupdate+

--- app/index.js
+++ app/index.js
@@ -1,23 +1,61 @@
-import { useState } from 'react';
-import { View, Text, ScrollView, SafeAreaView } from 'react-native';
+import { useEffect, useState } from 'react';
+import { View, Text, ScrollView, SafeAreaView, FlatList, TouchableOpacity } from 'react-native';
 import { Stack, useRouter, Redirect } from 'expo-router';
 import ScreenHeaderBtn from '../components/header/ScreenHeaderBtn';
-import { icons } from '../constants';
+import { icons,  } from '../constants';
 export default function Home() {
+
+  let [contactList, setContactList] = useState('')
+
+  useEffect(() =>{
+    
+  }, [])
+
+  const DATA = [
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+      title: 'First Item',
+    },
+    {
+      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+      title: 'Second Item',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Third Item',
+    },
+  ];
+  
+ 
+  const jobTypes = ["Full-time", "Part-time", "Contractor"];
   return(
-  <SafeAreaView style={{ flex: 1 }}>
+  <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
     <Stack.Screen
       options={{
         headerShadowVisible: false,
         headerTitle: '',
         headerLeft: () => (
+          <Text style={{fontSize: 20, fontWeight: 'bold'}} >Contact</Text>
+          ),
+          headerRight: () => (
           <ScreenHeaderBtn iconUrl={icons.chevronDark} dimension="50%" />
-        ),
-        headerRight: () => {
-        },
+
+        )
       }}
     />
-    <Text>als345dfj</Text>
+    <View style={{width: 900, height: 300}}>
+    <FlatList
+          data={DATA}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+
+            >
+              <Text >{item.id}</Text>
+            </TouchableOpacity>
+          )}
+          horizontal
+        />
+    </View>
   </SafeAreaView>
   )
 }
--- constants/index.js
+++ constants/index.js
@@ -2,4 +2,4 @@ import images from "./images";
 import icons from "./icons";
 import { COLORS, FONT, SIZES, SHADOWS } from "./theme";
 
-export { images, icons, COLORS, FONT, SIZES, SHADOWS };
+export { images, icons, COLORS, FONT, SIZES, SHADOWS, };
