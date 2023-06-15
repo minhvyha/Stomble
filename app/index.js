6967e0ede2c416d7d725f58f66bf998067195768
Minh Vy ha <minhvy.ha@outlook.com>
2023-06-15 15:08:46 +1000

    update

--- app/index.js
+++ app/index.js
@@ -1,66 +1,86 @@
-import { useEffect, useState } from 'react';
-import { View, Text, ScrollView, SafeAreaView, FlatList, TouchableOpacity } from 'react-native';
-import { Stack, useRouter, Redirect } from 'expo-router';
-import ScreenHeaderBtn from '../components/header/ScreenHeaderBtn';
-import { icons,  } from '../constants';
-export default function Home() {
+import React, {useState} from 'react';
+import {
+  FlatList,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
 
-  let [contactList, setContactList] = useState([])
 
-  useEffect(() =>{
-    async function fetchData(){
-      const response = await fetch('https://jsonplaceholder.typicode.com/posts')
-      const jsonResponse = await response.json()
-      setContactList(jsonResponse)
-    }
-    fetchData()
-  }, [])
 
-  const DATA = [
-    {
-      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-      title: 'First Item',
-    },
-    {
-      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-      title: 'Second Item',
-    },
-    {
-      id: '58694a0f-3da1-471f-bd96-145571e29d72',
-      title: 'Third Item',
-    },
-  ];
-  
- 
-  const jobTypes = ["Full-time", "Part-time", "Contractor"];
-  return(
-  <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
-    <Stack.Screen
-      options={{
-        headerShadowVisible: false,
-        headerTitle: '',
-        headerLeft: () => (
-          <Text style={{fontSize: 20, fontWeight: 'bold'}} >Contact</Text>
-          ),
-          headerRight: () => (
-          <ScreenHeaderBtn iconUrl={icons.chevronDark} dimension="50%" />
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+];
 
-        )
-      }}
-    />
-    <View style={{width: 900, height: 300}}>
-    <FlatList
-          data={contactList}
-          renderItem={({ item }) => (
-            <TouchableOpacity
+const Item = ({item, onPress, backgroundColor, textColor}) => (
+  <TouchableOpacity onPress={onPress} style={[styles.item, {backgroundColor}]}>
+    <Text style={[styles.title, {color: textColor}]}>{item.title}</Text>
+  </TouchableOpacity>
+);
 
-            >
-              <Text >{item.id}</Text>
-            </TouchableOpacity>
-          )}
-          horizontal
-        />
-    </View>
-  </SafeAreaView>
-  )
-}
+const App = () => {
+  const [selectedId, setSelectedId] = useState();
+
+  let [contactList, setContactList] = useState([]);
+  const getArticlesFromApi = async () => {
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    let json = await response.json();
+    setContactList(json);
+  };
+  getArticlesFromApi();
+
+  const renderItem = ({item}) => {
+    const backgroundColor = item.id === selectedId ? '#6e3b6e' : '#f9c2ff';
+    const color = item.id === selectedId ? 'white' : 'black';
+
+    return (
+      <Item
+        item={item}
+        onPress={() => setSelectedId(item.id)}
+        backgroundColor={backgroundColor}
+        textColor={color}
+      />
+    );
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={contactList}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+        extraData={selectedId}
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+});
+
+export default App;
