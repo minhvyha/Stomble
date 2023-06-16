b51004f637f65455797a0a41475781e2a26b7dd4
Minh Vy Ha <vyha0512@gmail.com>
2023-06-16 12:47:36 +1000

    udpate

--- app/contact/[id].js
+++ app/contact/[id].js
@@ -1,8 +1,22 @@
-import React from 'react'
+import { Stack, useRouter, useSearchParams } from "expo-router";
+import { useCallback, useState } from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  ActivityIndicator,
+  RefreshControl,
+} from "react-native";
 
 const ContactDetail = () => {
+  const params = useSearchParams()
+  const router = useRouter()
+
   return (
-    <div>ContactDetail</div>
+    <SafeAreaView>
+      
+    </SafeAreaView>
   )
 }
 
